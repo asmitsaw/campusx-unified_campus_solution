bbd57694af5451ec54cbaef88590ec3920d9a3fe
LitapTimus <sumitpatil141005@gmail.com>
2026-02-20 14:50:27 +0530

    lets go

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,8 @@ import authRoutes from "./routes/authroutes.js";
 import dashboardRoutes from "./routes/dashboardroutes.js";
 import eventRoutes from "./routes/eventroutes.js";
 import placementRoutes from "./routes/placementroutes.js";
-
+import attendanceRoutes from "./routes/attendanceRoutes.js";
+import materialRoutes from "./routes/materialRoutes.js"
 
 import libraryRoutes from "./routes/libraryRoutes.js";
 
@@ -21,9 +22,10 @@ app.use("/api/dashboard", dashboardRoutes);
 app.use("/api/events", eventRoutes);
 app.use("/api/placements", placementRoutes);
 app.use("/api/library", libraryRoutes);
+app.use("/api/attendance", attendanceRoutes);
+app.use("/api/materials", materialRoutes);
 
-
-app.get("/", (req, res) => {
+app.get("/", (req, res) => { 
   res.send("🚀 API Running with Supabase");
 });
 
