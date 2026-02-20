51654a7480148569681c6a34025c2980b03e0943
LitapTimus <sumitpatil141005@gmail.com>
2026-02-20 12:59:00 +0530

    niec

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ import authRoutes from "./routes/authroutes.js";
 import dashboardRoutes from "./routes/dashboardroutes.js";
 import eventRoutes from "./routes/eventroutes.js";
 import placementRoutes from "./routes/placementroutes.js";
+
+
 import libraryRoutes from "./routes/libraryRoutes.js";
 
 dotenv.config();
@@ -16,6 +18,9 @@ app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/dashboard", dashboardRoutes);
+app.use("/api/events", eventRoutes);
+app.use("/api/placements", placementRoutes);
+app.use("/api/library", libraryRoutes);
 
 
 app.get("/", (req, res) => {
